f60aa92beaccf48e9c6d3d4467d39f226e1031e3
Danny Hausmann <dgaming96@outlook.de>
2024-05-09 14:30:34 +0200

    Projektdateien hinzufügen.

---
+++ Pentago/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pentago
+{
+    public class Game
+    {
+        public Game() 
+        {
+            Intit();
+        }
+
+        private void Intit()
+        {
+            
+        }
+    }
+}
---
+++ Pentago/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pentago
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Button_1.Background = Brushes.Yellow;
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            GridTopR.RenderTransform = new RotateTransform(90);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+            GridTopR.RenderTransform = new TranslateTransform(GridTopR.get + 90)
+        }
+    }
+}
