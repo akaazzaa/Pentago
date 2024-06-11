927677e094abf12f0673102a622405e5358daff2
Danny Hausmann <dgaming96@outlook.de>
2024-06-11 21:13:47 +0200

    ..

--- Pentago/Klassen/ComputerGegner.cs
+++ Pentago/Klassen/ComputerGegner.cs
@@ -18,38 +18,9 @@ namespace Pentago.Klassen
             GameBoardCopy = new Player[6,6];
             CurrentPlayer = Player.Blue;
         }
-
-        public int Minimax(int depth, bool isMaximizingPlayer)
-        {
-            if (depth == 0)
-            {
-                return EvaluateBoard();
-            }
-
-            List<Tuple<int, int, Corner, Direction>> allMoves = GetAllMoves();
-            int bestScore = isMaximizingPlayer ? int.MinValue : int.MaxValue;
-
-            foreach (var move in allMoves)
-            {
-                int row = move.Item1;
-                int col = move.Item2;
-                Corner corner = move.Item3;
-                Direction direction = move.Item4;
-
-                SimulateMove(row, col, corner, direction);
-                int score = Minimax(depth - 1, !isMaximizingPlayer);
-                DeletMove(row, col, corner, direction);
-
-                bestScore = isMaximizingPlayer ? Math.Max(bestScore, score) : Math.Min(bestScore, score);
-            }
-
-            return bestScore;
-        }
-        private int EvaluateBoard()
-        {
-            throw new NotImplementedException();
-        }
-        private List<Tuple<int, int, Corner, Direction>> GetAllMoves()
+  
+     
+        public List<Tuple<int, int, Corner, Direction>> GetAllMoves()
         {
             List<Tuple<int, int, Corner, Direction>> moves = new List<Tuple<int, int, Corner, Direction>>();
 
@@ -69,98 +40,24 @@ namespace Pentago.Klassen
             }
             return moves;
         }
-        private Player[,] Rotate3x3(Player[,] tmp, Direction direction)
+   
+       private void Maximieren(int tiefe)
         {
-            Player[,] rotated = new Player[3, 3];
-            if (direction == Direction.Right)
+
+                
+
+
+            foreach( var moves in GetAllMoves() )
             {
-                for (int r = 0; r < 3; r++)
-                {
-                    for (int c = 0; c < 3; c++)
-                    {
-                        rotated[r, c] = tmp[2 - c, r];
-                    }
-                }
+
+                Minimieren(tiefe - 1);
+
             }
-            else if (direction == Direction.Left)
-            {
-                for (int r = 0; r < 3; r++)
-                {
-                    for (int c = 0; c < 3; c++)
-                    {
-                        rotated[r, c] = tmp[c, 2 - r];
-                    }
-                }
-            }
-            else
-            {
-                return null;
-            }
-            return rotated;
-        }
-        public Player[,] SimulateRotation(Corner corner, Direction direction)
-        {
-            int rowStart, colStart;
-            switch (corner)
-            {
-                case Corner.Topleft:
-                    rowStart = 0;
-                    colStart = 0;
-                    break;
-                case Corner.Topright:
-                    rowStart = 0;
-                    colStart = 3;
-                    break;
-                case Corner.Botleft:
-                    rowStart = 3;
-                    colStart = 0;
-                    break;
-                case Corner.Botright:
-                    rowStart = 3;
-                    colStart = 3;
-                    break;
-                default:
-                    return null;
-            }
-
-
-            Player[,] tmp = new Player[3, 3];
-            for (int r = 0; r < 3; r++)
-            {
-                for (int c = 0; c < 3; c++)
-                {
-                    tmp[r, c] = GameBoardCopy[rowStart + r, colStart + c];
-                }
-            }
-
-
-            Player[,] rotatedarray = Rotate3x3(tmp, direction);
-
-
-            for (int r = 0; r < 3; r++)
-            {
-                for (int c = 0; c < 3; c++)
-                {
-                    GameBoardCopy[rowStart + r, colStart + c] = rotatedarray[r, c];
-                }
-            }
-
-            return GameBoardCopy;
-        }
-        private void DeletMove(int row, int col, Corner corner, Direction direction)
-        {
-            GameBoardCopy[row, col] = Player.None;
-
-            // Drehe den Quadranten in die entgegengesetzte Richtung zurück
-            SimulateRotation(corner, direction == Direction.Right ? Direction.Left : Direction.Right);
-        }
-        private void SimulateMove(int row, int col, Corner corner, Direction direction)
-        {
-            GameBoardCopy[row, col] = CurrentPlayer;
-
-            SimulateRotation(corner, direction);
-
         }
 
+        private void Minimieren(int v)
+        {
+            
+        }
     }
 }
